8353f34198796e5f66e2c8eed0318a7fa59ed68b
Haobin Liu （刘浩彬） <liuhaobin@trip.com>
2025-05-15 18:59:28 +0800

    初始化代码

---
+++ agent-romance/src/main/java/com/haobin/agentromance/AgentRomanceApplication.java
@@ -0,0 +1,13 @@
+package com.haobin.agentromance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentRomanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentRomanceApplication.class, args);
+    }
+
+}
---
+++ agent-romance/src/test/java/com/haobin/agentromance/AgentRomanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haobin.agentromance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentRomanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
